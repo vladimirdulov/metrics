4936766a13d71d187301dbf280f8b54113a49f9b
Vladimir Dulov <vladimir@brandlight.org>
2022-12-07 18:38:27 +0400

    further code tidying up

--- metric.sh
+++ metric.sh
@@ -5,24 +5,23 @@ API_PATH='/api/count'
 
 # if we really need to start from the first second of the minute
 #
-#if [ $(date +"%S") != "00" ]
-#then
+#if [ $(date +"%S") != "00" ]; then
 #	echo "Waiting a new minute begins"
-#	while [ $(date +"%S") != "00" ]; do echo -n "."; sleep 1; done
+#	while [ $(date +"%S") != "00" ]; do
+#		echo -n "."
+#		sleep 1
+#	done
 #	echo ""
 #fi
 
-while true
-do
-	for site in "${sites[@]}"
-	do
-		response=$(curl --connect-timeout 3 -s -w "\n%{http_code}" "http://${site}${API_PATH}")
-		http_code=$(tail -n1 <<< "$response")
-		if [ "$http_code" != 200 ]
-		then
+while true; do
+	for site in "${sites[@]}"; do
+		response=$(curl --connect-timeout 10 -s -w "\n%{http_code}" "http://${site}${API_PATH}")
+		http_code=$(tail -n1 <<<"$response")
+		if [ "$http_code" != 200 ]; then
 			continue
 		fi
-		metric=$(sed '$ d' <<< "$response" | sed 's/[^0-9]*//g')
+		metric=$(sed '$ d' <<<"$response" | sed 's/[^0-9]*//g')
 		echo $(date +'%Y-%m-%d %H:%M:%S') ${site} ${metric}
 	done
 
