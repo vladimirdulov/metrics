eee724844d5d76c52ce85da5387fb76e063b2fbb
Vladimir Dulov <vladimir@brandlight.org>
2022-12-07 18:31:41 +0400

    code tidying up

--- start_server.sh
+++ start_server.sh
@@ -2,34 +2,29 @@
 
 sites=('maria.ru' 'rosa.ru' 'sina.ru')
 
-for site in "${sites[@]}"
-do
+for site in "${sites[@]}"; do
 	ip=$(ping -q -c 1 -t 1 $site 2>/dev/null | grep PING | sed -e "s/).*//" | sed -e "s/.*(//")
-	if [ "$ip" != '127.0.0.1' ]
-	then
+	if [ "$ip" != '127.0.0.1' ]; then
 		echo "Make sure you have added a record \"127.0.0.1 $site\" to /etc/hosts"
 		exit
 	fi
 done
 
-if [ ! -f server/www/api.php ]
-then
-	mkdir -p server/www;
-	cat << EOF > server/www/api.php
+if [ ! -f server/www/api.php ]; then
+	mkdir -p server/www
+	cat <<EOF >server/www/api.php
 <?php
 srand();
 echo "{\"count\": " . rand(10, 99) . "}";
 EOF
 fi
 
-if [ ! -f server/nginx/hosts.conf ]
-then
+if [ ! -f server/nginx/hosts.conf ]; then
 	mkdir -p server/nginx
-# && touch server/nginx/hosts.conf
-	for site in "${sites[@]}"
-	do
-#		cat <<eof >> server/nginx/hosts.conf
-		cat <<EOF >> server/nginx/$site.conf
+	# && touch server/nginx/hosts.conf
+	for site in "${sites[@]}"; do
+		#		cat <<eof >> server/nginx/hosts.conf
+		cat <<EOF >>server/nginx/$site.conf
 server {
 	listen 8080;
 	listen [::]:8080;
@@ -61,6 +56,6 @@ fi
 
 docker run \
 	-p 80:8080 \
-	-v "`pwd`/server/www:/var/www/html" \
-	-v "`pwd`/server/nginx:/etc/nginx/conf.d" \
+	-v "$(pwd)/server/www:/var/www/html" \
+	-v "$(pwd)/server/nginx:/etc/nginx/conf.d" \
 	trafex/php-nginx
